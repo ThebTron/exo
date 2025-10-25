e69469b7fb481c6b5f599ff53d1dee7a575d1a8f
thebtron <alex.thebelt@googlemail.com>
2025-10-25 16:56:56 +0100

    move code

--- exo/main.py
+++ exo/main.py
@@ -16,6 +16,7 @@ import uvloop
 from exo import DEBUG
 from exo.helpers import ensure_endpoint_alive
 from exo.runner import ExoChatRunner, ExoRayComputeRunner
+from exo.runner.ray_compute import submit_ray_job
 
 # TODO: figure out why this is happening
 os.environ["GRPC_VERBOSITY"] = "error"
@@ -57,48 +58,8 @@ def run_ray():
     exo_ray = ExoRayComputeRunner.from_cli()
     run(runner=exo_ray)
 
-def submit_ray_job():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("python_file_path", type=str)
-    parser.add_argument("--api-port", type=int, default=52415)
-    parser.add_argument("--setup-wait", type=int, default=2)
-    args = parser.parse_args()
-
-    _filepath = Path(args.python_file_path)
-
-    if _filepath.suffix != ".py":
-       raise ValueError(f"Only python scripts are supported as valid ray jobs")
-
-    # kill other ray cluster
-    ensure_endpoint_alive(args.api_port)
-    response = requests.post(f"http://localhost:{args.api_port}/stop_ray").json()
-
-    time.sleep(args.setup_wait)
-
-    # start fresh ray cluster
-    response = requests.post(f"http://localhost:{args.api_port}/start_ray").json()    
-
-    time.sleep(args.setup_wait)
-
-    if DEBUG >= 1:
-        print(response)
-
-    if response["status"] == "failure":
-        raise HTTPException(
-            f"Starting ray failed, message: {response['message']}"
-        )
-    
-    # get job submission client
-    head_node_address = response["message"]["head_node_address"]
-    client = JobSubmissionClient(head_node_address)
-    _entrypoint = f"python {_filepath}"
-    job_id = client.submit_job(entrypoint=_entrypoint)
-
-    print(f"Submitted ({job_id=}): {_entrypoint}")
-
-    info = ray.init("auto", logging_level="ERROR")
-    print(f"Job queue is available at: http://{info['webui_url']}")
-
+def submit_ray_job_cmd():
+   submit_ray_job()
 
 if __name__ == "__main__":
   run_chat()
--- exo/runner/ray_compute.py
+++ exo/runner/ray_compute.py
@@ -1,17 +1,21 @@
+import argparse
 import json
 import os
+from pathlib import Path
 import subprocess
 import time
 import uuid
 
 import asyncio
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import Field
 import ray
+from ray.dashboard.modules.job.sdk import JobSubmissionClient
+import requests
 import uvicorn
 
 from exo import DEBUG
-from exo.helpers import print_yellow_exo, get_local_ip
+from exo.helpers import get_local_ip, ensure_endpoint_alive
 from exo.orchestration import RayComputeNode
 from exo.runner import ExoRunner
 from exo.viz.topology_viz import TopologyViz
@@ -169,3 +173,45 @@ class ExoRayComputeRunner(ExoRunner):
     async def run(self):
         await self._api.serve()
         
+
+def submit_ray_job():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("python_file_path", type=str)
+    parser.add_argument("--api-port", type=int, default=52415)
+    parser.add_argument("--setup-wait", type=int, default=2)
+    args = parser.parse_args()
+
+    _filepath = Path(args.python_file_path)
+
+    if _filepath.suffix != ".py":
+       raise ValueError(f"Only python scripts are supported as valid ray jobs")
+
+    # kill other ray cluster
+    ensure_endpoint_alive(args.api_port)
+    response = requests.post(f"http://localhost:{args.api_port}/stop_ray").json()
+
+    time.sleep(args.setup_wait)
+
+    # start fresh ray cluster
+    response = requests.post(f"http://localhost:{args.api_port}/start_ray").json()    
+
+    time.sleep(args.setup_wait)
+
+    if DEBUG >= 1:
+        print(response)
+
+    if response["status"] == "failure":
+        raise HTTPException(
+            f"Starting ray failed, message: {response['message']}"
+        )
+    
+    # get job submission client
+    head_node_address = response["message"]["head_node_address"]
+    client = JobSubmissionClient(head_node_address)
+    _entrypoint = f"python {_filepath}"
+    job_id = client.submit_job(entrypoint=_entrypoint)
+
+    print(f"Submitted ({job_id=}): {_entrypoint}")
+
+    info = ray.init("auto", logging_level="ERROR")
+    print(f"Job queue is available at: http://{info['webui_url']}")
